ccb1577b7fb27fa60b8504f95cac26b5ace1020d
GabrielMadMimic <65048774+GabrielMadMimic@users.noreply.github.com>
2022-05-31 13:30:42 -0300

    Fix null reference when recive empty data

--- TicTacToeUnity/Assets/_Project/Scripts/ContractData.cs
+++ TicTacToeUnity/Assets/_Project/Scripts/ContractData.cs
@@ -14,48 +14,14 @@ namespace Tokenage
         public string name;
         public string address;
         public TokenData[] nfts;
-        public string image;
-        public string id;
-        public string category;
-        public string attributes;
-        public string tokenId;
-        public string owner;
-        public string ownerName;
-        public string contract;
-        public string contractName;
-        public string contractFriendlyName;
-        public string gameId;
-        public string gameName;
-        public string commission;
-        public string providerId;
-        public string providerName;
-        public bool minted;
 
-        public ContractData(string _symbol, string _friendlyName, string _name, string _address, TokenData[] _nfts, string _image,
-            string _id, string _category, string _attributes, string _tokenId, string _owner, string _ownerName, string _contract,
-            string _contractName, string _contractFriendlyName, string _gameId, string _gameName, string _commision, string _providerId,
-            string _providerName, bool _minted)
+        public ContractData(string _symbol, string _friendlyName, string _name, string _address, TokenData[] _nfts)
         {
             symbol = _symbol;
             friendlyName = _friendlyName;
             name = _name;
             address = _address;
             nfts = _nfts;
-            image = _image;
-            id = _id;
-            category = _category;
-            attributes = _attributes;
-            tokenId = _tokenId;
-            owner = _owner;
-            ownerName = _ownerName;
-            contract = _contract;
-            contractFriendlyName = _contractFriendlyName;
-            gameId = _gameId;
-            gameName = _gameName;
-            commission = _commision;
-            providerId = _providerId;
-            providerName = _providerName;
-            minted = _minted;
         }
     }
 }
